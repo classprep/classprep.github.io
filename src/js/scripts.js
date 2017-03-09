0afd2780c9af41d6c0cdf51e50f1556115725284
dvmoomoodv <dvmoomoodv@gmail.com>
2017-03-09 12:51:44 +0900

    page add

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,23 @@
 // Empty JS for your own code to be here
 
+document.addEventListener('click', doThing);
+
+function doThing(event) {
+  var el;
+
+  // we can check the tag type, and if it's not the <a>, move up.
+  if (event.target.tagType == "rect") {
+    // move up TWICE
+    el = event.target.parentElement.parentElement;
+  } else if (event.target.tagType == "svg") {
+    // move up ONCE
+    el = event.target.parentElement;
+  } else {
+    el = event.target;
+  }
+  console.log(el.getAttribute("data-data"));
+}
+
 
 $(function(){
     $("#logosvg").click(function(){
